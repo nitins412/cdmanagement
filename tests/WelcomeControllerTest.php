047a3f6ec0bad5c11314a5cfbceaf6ad662acc20
SHRIVASTAV Nitin TGI/OLN <hdsq8519@orange.com>
2018-11-03 17:30:19 +0530

    added files for test

---
+++ tests/WelcomeControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WelcomeController extends \PHPUnit_Framework_TestCase{
+	/**
+	 * Show a list of all of the CD.
+	 *
+	 * @return Response
+	 */
+	
+
+	/**
+	 * $cdCollection = DB::select ( 'select * from cd_collection' );
+	 */
+	public function testCdCollectionDBSelectSelectFromCdCollection()
+	{
+		$this->markTestIncomplete('implement me...');
+	}
+
+	/**
+	 * public function getcddetail(Request $request) {
+	 */
+	public function testPublicFunctionGetcddetailRequestRequest()
+	{
+		$this->markTestIncomplete('implement me...');
+	}
+
+}
